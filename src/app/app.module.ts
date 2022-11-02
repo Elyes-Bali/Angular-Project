650d19ed60611cc89b31829a094a6738f06ec9f0
zAYNOSz <baliaynos@gmail.com>
2022-11-03 00:38:24 +0100

    NextV14

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbareComponent } from './components/navbare/navbare.component';
+import { HomeComponent } from './components/home/home.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { MenupageComponent } from './components/menupage/menupage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavbareComponent,
+    HomeComponent,
+    MenuComponent,
+    AboutComponent,
+    ContactComponent,
+    MenupageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/navbare/navbare.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbare',
+  templateUrl: './navbare.component.html',
+  styleUrls: ['./navbare.component.scss']
+})
+export class NavbareComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
